a23c6501e26b2d9c8884ee520d37304907dae62b
thiagosena-avsystemgeo <thiagosena@avsystemgeo.com.br>
2019-05-30 16:33:35 -0300

    Spring Initializr

---
+++ spring-boot-mongodb/src/test/java/com/thiagorsena/springmongodb/SpringmongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thiagorsena.springmongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringmongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
